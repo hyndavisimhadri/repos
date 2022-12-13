afb84c82592bcc8cbc7bb42c0785dd7d9a76c758
hyndavisimhadri <115202998+hyndavisimhadri@users.noreply.github.com>
2022-12-13 09:12:36 +0000

    Add files via upload

---
+++ UserController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CrudOperations.Models;
+using CrudOperations.Services;
+
+namespace CrudOperations.Controllers
+{
+    public class UserController : ApiController
+    {
+        private UserServices _addServices;
+
+        public UserController()
+        {
+            _addServices = new UserServices();
+        }
+        public UserController(UserServices addServices)
+        {
+            _addServices = addServices;
+        }
+
+
+        public string Post(UserTable user)
+        {
+            _addServices.Post(user);
+            return "Customer Inserted successfully";
+        }
+
+        public IEnumerable<UserTable> GetAll()
+        {
+            return _addServices.GetAll();
+        }
+
+        public string Put(UserTable user)
+        {
+            _addServices.Put(user);
+            return "Customers updated";
+        }
+        public string Delete(int ID)
+        {
+            _addServices.Delete(ID);
+
+            return "customres deleted";
+        }
+        public UserTable GetbyId(int ID)
+        {
+            return (_addServices.GetbyId(ID));
+
+
+        }
+    }
+}
